494940308168faf103cd77ff4d0eccc7930ec440
davik20 <onyejivic@gmail.com>
2023-06-13 21:50:43 +0100

    update

--- hooks/useClickOutside.ts
+++ hooks/useClickOutside.ts
@@ -3,30 +3,32 @@ type RefType = HTMLElement | null;
 
 
 const useClickOutside = <T extends RefType>(
-    ref: React.RefObject<T>,
-    handler: (event: MouseEvent | TouchEvent) => void
-  ) => {
-    const savedHandler = useRef(handler);
+  ref: React.RefObject<T>,
+  handler: (event: MouseEvent | TouchEvent) => void
+) => {
+  const savedHandler = useRef(handler);
 
-    useEffect(() => {
-      savedHandler.current = handler;
-    }, [handler]);
-  
-    useEffect(() => {
-      const handleClickOutside = (event: MouseEvent | TouchEvent) => {
-        if (ref.current && !ref.current.contains(event.target as Node)) {
-          savedHandler.current(event);
-        }
-      };
-  
-      document.addEventListener("mousedown", handleClickOutside);
-      document.addEventListener("touchstart", handleClickOutside);
-  
-      return () => {
-        document.removeEventListener("mousedown", handleClickOutside);
-        document.removeEventListener("touchstart", handleClickOutside);
-      };
-    }, [ref]);
-  };
-  
-  export default useClickOutside;
+  useEffect(() => {
+
+
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent | TouchEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        savedHandler.current(event);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("touchstart", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("touchstart", handleClickOutside);
+    };
+  }, [ref]);
+};
+
+export default useClickOutside;
